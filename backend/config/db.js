19c3e0fc65158d227a84315d155efa3b37ac0505
aya-anwar22 <aya786930@gmail.com>
2025-06-27 14:23:46 +0300

    backend and frontend

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,31 @@
+// const mongoose = require('mongoose');
+
+// const dbConnect = () =>{
+//     mongoose.connect(process.env.DB_URI)
+//     .then((conn) =>{
+//         console.log(`DataBase connected ${conn.connection.host}`)
+//     }).catch((err)=>{
+//         console.log(err);
+//         process.exit(1)
+//     })
+// }
+
+
+// module.exports = dbConnect;
 const mongoose = require('mongoose');
 
-const dbConnect = () =>{
-    mongoose.connect(process.env.DB_URI)
-    .then((conn) =>{
-        console.log(`DataBase connected ${conn.connection.host}`)
-    }).catch((err)=>{
-        console.log(err);
-        process.exit(1)
-    })
-}
+const dbConnect = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
 
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.error(`MongoDB Connection Error: ${err.message}`);
+    process.exit(1); 
+  }
+};
 
 module.exports = dbConnect;
