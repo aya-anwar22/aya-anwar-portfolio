3f3cf4e4421c8ec0557f206c110fc42911ca266b
aya-anwar22 <aya786930@gmail.com>
2025-06-27 14:04:26 +0300

    backend

--- models/skills.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const skillSchema = new mongoose.Schema({
-    title: String,
-    items: [String]
-})
-const Skill = mongoose.model('Skill',skillSchema )
-module.exports = Skill
--- models/socialLinks.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const socialLinksSchema = new mongoose.Schema({
-    platform: String,
-    url: String
-})
-const SocialLinks = mongoose.model('SocialLinks',socialLinksSchema )
-module.exports = SocialLinks
