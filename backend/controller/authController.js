df33a3adb9944af96e40fe6f6b78e8262e05f681
aya-anwar22 <aya786930@gmail.com>
2025-06-29 09:15:09 +0300

    frontend

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -20,7 +20,7 @@ async function generateTokens(user, regenerateRefreshToken = false) {
         refreshToken = jwt.sign(
             { userId: user.id },
             process.env.REFRESH_TOKEN_SECRET,
-            { expiresIn: "40d" }
+            { expiresIn: "10d" }
         );
         refreshTokenExpiry = new Date(Date.now() + 10 * 24 * 60 * 60 * 1000);
 
