0364f042fac18d4e9397f88821aa9e2ac7b8b76c
aya-anwar22 <aya786930@gmail.com>
2025-07-16 19:50:17 +0300

    en

--- frontend/src/app/features/services/auth.service.ts
+++ frontend/src/app/features/services/auth.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, tap } from 'rxjs';
-import { environment } from '../../../environments/environment.prod'
+// import { environment } from '../../../environments/environment.prod'
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
- private apiUrl = `${environment.apiUrl}/auth`;
+ private apiUrl = `https://aya-anwar-portfolio-danr.vercel.app/api/v1/auth`;
   constructor(private http: HttpClient) {}
 
   login(data: { email: string; password: string }): Observable<any> {
